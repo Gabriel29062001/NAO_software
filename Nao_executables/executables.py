b9b3299b849aab9e83c677d7d047699c1a060edb
Gabriel <gabriel.paffi@yahoo.com>
2025-01-29 16:51:26 +0100

    Last version of exe

--- Nao_executables/executables.py
+++ Nao_executables/executables.py
@@ -6,155 +6,136 @@ import os
 import subprocess
 
 
+class NaoLauncher:
+    def __init__(self):
+        """Initialisation de l'application NAO"""
+        self.system, self.delimitations = self.detecter_systeme()
+        self.current_path = self.trouver_dossier_nao()
+        
+        self.creches = ["La_Nanosphère", "Les_Petits_Acrobates", "Les_6_sens", "Le_Microcosme", "Vivalys", "HOME", "Rachel"]
+        self.creches_ROBOT_IP = ['172.24.24.150', "192.168.55.60", "192.168.1.69", "192.168.51.159", "172.16.40.105", "10.101.0.186", "172.20.10.5"]
 
-def detecter_systeme():
+        self.selected_choice = None
+        self.selected_choice_tasks = None
+        self.volume = None
 
-    systeme = platform.system()
-    if systeme == "Windows":
-        return "windows", "\\"
-    elif systeme == "Linux":
-        return "linux", "/"
+        self.demander_structure()
 
-system, delimitations = detecter_systeme()
-current_path = os.getcwd()
+    @staticmethod
+    def detecter_systeme():
+        """Détecte le système d'exploitation et retourne le bon séparateur de fichiers."""
+        systeme = platform.system()
+        return ("windows", "\\") if systeme == "Windows" else ("linux", "/")
 
-# Define the target folder
-target_folder = "nao_softwarev3"
+    def trouver_dossier_nao(self):
+        """Trouve le répertoire racine du projet NAO."""
+        current_path = os.getcwd()
+        target_folder = "nao_softwarev3"
 
-# Iterate until the target folder is found
-while os.path.basename(current_path) != target_folder:
-    current_path = os.path.dirname(current_path)
+        while os.path.basename(current_path) != target_folder:
+            current_path = os.path.dirname(current_path)
 
-selected_choice = None
-selected_choice_tasks = None
+        return current_path
 
+    def demander_structure(self):
+        """Affiche une fenêtre pour sélectionner la crèche."""
+        self.fenetre = tk.Tk()
+        self.fenetre.geometry("400x250")
+        self.fenetre.title("NAO")
 
-def afficher_choix():
-    global selected_choice
-    selected_choice = curseur.get()
-    print("La réponse choisie est : " + selected_choice)
-    fenetre.destroy()
+        question_label = tk.Label(self.fenetre, text="Dans quelle structure êtes-vous ?")
+        question_label.pack(pady=10)
 
-def afficher_choix_tasks():
-    global selected_choice_tasks
-    selected_choice_tasks = curseur.get()
-    print("La réponse choisie est : " + selected_choice_tasks)
-    fenetre_tasks.destroy()
+        self.curseur = tk.StringVar()
+        for i, creche in enumerate(self.creches):
+            tk.Radiobutton(self.fenetre, text=creche, variable=self.curseur, value=i).pack()
 
+        bouton_afficher = tk.Button(self.fenetre, text="Sélectionner réponse", command=self.afficher_choix)
+        bouton_afficher.pack(pady=10)
 
-# Créer une fenêtre
-fenetre = tk.Tk()
-fenetre.geometry("400x250")
-fenetre.title("NAO")
+        self.fenetre.mainloop()
 
-# Créer un label pour afficher la question
-question_label = tk.Label(fenetre, text="Dans quels structure êtes vous ?")
-question_label.pack(pady=10)
+    def afficher_choix(self):
+        """Récupère le choix de la crèche et exécute le script correspondant si besoin."""
+        self.selected_choice = int(self.curseur.get())
+        print(f"Structure sélectionnée : {self.creches[self.selected_choice]}")
+        self.fenetre.destroy()
 
-# Créer un curseur (Radiobutton) avec les choix possibles
-curseur = tk.StringVar()
+        if self.selected_choice == 6:  # Cas spécifique pour "Défi des 10"
+            fichier_output = "/home/gabriel/Documents/nao/nao_softwarev3/Nao_tasks/defi_10/defi_10.py"
+            print(fichier_output)
+            subprocess.call(["python2", fichier_output, str(self.creches_ROBOT_IP[self.selected_choice])])
+            exit()
 
-choix_a = tk.Radiobutton(fenetre, text="Nanosphère", variable=curseur, value=0)
-choix_b = tk.Radiobutton(fenetre, text="Petits Acrobates", variable=curseur, value=1)
-choix_c = tk.Radiobutton(fenetre, text="Les 6 sens", variable=curseur, value=2)
-choix_d = tk.Radiobutton(fenetre, text="Le Microcosme", variable=curseur, value=3)
-choix_e = tk.Radiobutton(fenetre, text="Vivalys", variable=curseur, value=4)
-choix_f = tk.Radiobutton(fenetre, text="Home", variable=curseur, value=5)
-choix_g = tk.Radiobutton(fenetre, text="Défi des 10", variable=curseur, value=6)
+        self.demander_type_tache()
 
+    def demander_type_tache(self):
+        """Affiche une fenêtre pour sélectionner le type de tâche de NAO."""
+        self.fenetre_tasks = tk.Tk()
+        self.fenetre_tasks.geometry("400x200")
+        self.fenetre_tasks.title("NAO")
 
-choix_a.pack()
-choix_b.pack()
-choix_c.pack()
-choix_d.pack()
-choix_e.pack()
-choix_f.pack()
-choix_g.pack()
+        question_label = tk.Label(self.fenetre_tasks, text="Quel type de tâche doit-il effectuer ?")
+        question_label.pack(pady=10)
 
-# Créer un bouton pour afficher le choix
-bouton_afficher = tk.Button(fenetre, text="Sélectionner réponse", command=afficher_choix)
-bouton_afficher.pack(pady=10)
+        self.curseur_tasks = tk.StringVar()
+        choix_taches = [("General Tasks", "general"), ("Thèmes crèches", "themes"), ("Tests", "tests"), ("Changer le Volume", "volume")]
 
+        for texte, valeur in choix_taches:
+            tk.Radiobutton(self.fenetre_tasks, text=texte, variable=self.curseur_tasks, value=valeur).pack()
 
-creches = ["La_Nanosphère","Les_Petits_Acrobates", "Les_6_sens", "Le_Microcosme", "Vivalys", "HOME", "Rachel"]
-creches_ROBOT_IP = ['172.24.24.150', "192.168.55.60","192.168.1.69", "192.168.51.159",  "172.16.40.105", "10.101.0.186", "172.20.10.5"]
+        bouton_afficher = tk.Button(self.fenetre_tasks, text="Sélectionner réponse", command=self.afficher_choix_tasks)
+        bouton_afficher.pack(pady=10)
 
-# Boucle principale
-fenetre.mainloop()
-print(creches[int(selected_choice)])
+        self.fenetre_tasks.mainloop()
 
-if (int(selected_choice))==6:
-    fichier_output = "/home/gabriel/Documents/nao/nao_softwarev3/Nao_tasks/defi_10/defi_10.py"
-    print(fichier_output)
-    subprocess.call(["python2",fichier_output, str(creches_ROBOT_IP[int(selected_choice)])])
-    exit()
+    def afficher_choix_tasks(self):
+        """Récupère le choix de la tâche et exécute l'action correspondante."""
+        self.selected_choice_tasks = self.curseur_tasks.get()
+        print(f"Tâche sélectionnée : {self.selected_choice_tasks}")
+        self.fenetre_tasks.destroy()
 
-# Créer une fenêtre
-fenetre_tasks = tk.Tk()
-fenetre_tasks.geometry("400x200")
-fenetre_tasks.title("NAO")
+        if self.selected_choice_tasks == "volume":
+            self.demander_volume()
+        else:
+            self.executer_script()
 
-# Créer un label pour afficher la question
-question_label = tk.Label(fenetre_tasks, text="Quels type de tache doit-il effectuer ?")
-question_label.pack(pady=10)
+    def demander_volume(self):
+        """Affiche une fenêtre pour sélectionner le volume de NAO."""
+        self.fenetre_vol = tk.Tk()
+        self.fenetre_vol.geometry("400x200")
+        self.fenetre_vol.title("Volume de NAO")
 
+        self.volume_value = tk.IntVar()
 
-# Créer un curseur (Radiobutton) avec les choix possibles
-curseur = tk.StringVar()
+        def update_percentage(val):
+            volume_label.config(text=f"Volume : {self.volume_value.get()} %")
 
-choix_a = tk.Radiobutton(fenetre_tasks, text="General Tasks", variable=curseur, value="general")
-choix_b = tk.Radiobutton(fenetre_tasks, text="Thèmes crèches", variable=curseur, value="themes")
-choix_c = tk.Radiobutton(fenetre_tasks, text="Tests", variable=curseur, value="tests")
-choix_d = tk.Radiobutton(fenetre_tasks, text="Changer le Volume", variable=curseur, value="volume")
+        def valider_volume():
+            """Valide et applique le volume sélectionné."""
+            self.volume = self.volume_value.get()
+            print(f"Volume sélectionné : {self.volume} %")
+            fichier_output = "/home/gabriel/Documents/nao/nao_softwarev3/Nao_tasks/volume/volume.py"
+            subprocess.call(["python2", fichier_output, str(self.creches_ROBOT_IP[self.selected_choice]), str(self.volume)])
 
+        curseur_volume = tk.Scale(self.fenetre_vol, from_=0, to=100, orient="horizontal", variable=self.volume_value, command=update_percentage)
+        curseur_volume.pack(pady=10)
 
-choix_a.pack()
-choix_b.pack()
-choix_c.pack()
-choix_d.pack()
+        volume_label = tk.Label(self.fenetre_vol, text="Volume : 0 %")
+        volume_label.pack(pady=10)
 
-# Créer un bouton pour afficher le choix
-bouton_afficher = tk.Button(fenetre_tasks, text="Sélectionner réponse", command=afficher_choix_tasks)
-bouton_afficher.pack(pady=10)
+        bouton_valider = tk.Button(self.fenetre_vol, text="Valider", command=valider_volume)
+        bouton_valider.pack(pady=10)
 
+        self.fenetre_vol.mainloop()
 
-fenetre_tasks.mainloop()
-
-if selected_choice_tasks =="volume":
-
-    fenetre_vol = tk.Tk()
-    fenetre_vol.geometry("400x200")
-    fenetre_vol.title("Volume de NAO")
-
-    volume_value = tk.IntVar()
-
-    # Fonction pour mettre à jour l'affichage du pourcentage
-    def update_percentage(val):
-        volume_label.config(text=f"Volume : {volume_value.get()} %")
-
-    def valider_volume():
-        print(f"Le volume sélectionné est : {volume_value.get()} %")
-        global volume
-        volume = volume_value.get()
-        print(volume)
-        fichier_output = "/home/gabriel/Documents/nao/nao_softwarev3/Nao_tasks/volume/volume.py"
-        subprocess.call(["python2",fichier_output, str(creches_ROBOT_IP[int(selected_choice)]), str(volume)])
-
-    curseur_volume = tk.Scale(fenetre_vol, from_=0, to=100, orient="horizontal", variable=volume_value, command=update_percentage)
-    curseur_volume.pack(pady=10)
-
-    volume_label = tk.Label(fenetre_vol, text="Volume : 0 %")
-    volume_label.pack(pady=10)
-    
-    bouton_valider = tk.Button(fenetre_vol, text="Valider", command=valider_volume)
-    bouton_valider.pack(pady=10)
-    
-    fenetre_vol.mainloop()
+    def executer_script(self):
+        """Exécute le script correspondant au type de tâche choisi."""
+        fichier_output = os.path.join(self.current_path, "Nao_tasks", self.selected_choice_tasks, f"{self.selected_choice_tasks}.py")
+        print(f"Exécution du script : {fichier_output}")
+        subprocess.call(["python3", fichier_output, str(self.creches_ROBOT_IP[self.selected_choice])])
 
 
-
-    
-else :
-    fichier_output = current_path + delimitations + "Nao_tasks" + delimitations + selected_choice_tasks + delimitations + selected_choice_tasks + ".py"
-    print(fichier_output)
-    subprocess.call(["python3",fichier_output, str(creches_ROBOT_IP[int(selected_choice)])])
+# Lancer l'application
+if __name__ == "__main__":
+    NaoLauncher()
